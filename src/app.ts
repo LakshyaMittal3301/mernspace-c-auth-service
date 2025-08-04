a27d18c0c2288e0d83f95009faf86549c3ffdcad
Lakshya Mittal <lakshyamittalaka@gmail.com>
2025-08-04 20:43:57 +0530

    Add global error handler and remove eslint

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,26 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
+import { HttpError } from "http-errors";
+import logger from "./config/logger";
 
 const app = express();
 
-app.get("/ping", (req, res) => {
+app.get("/ping", async (req, res) => {
     res.send("PONG (AUTH Service)");
 });
 
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+    logger.error(err.message);
+    const statusCode = err.statusCode || 500;
+
+    res.status(statusCode).json({
+        errors: [
+            {
+                type: err.name,
+                msg: err.message,
+                path: "",
+                location: "",
+            },
+        ],
+    });
+});
 export default app;
