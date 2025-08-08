bc6a67ff4c6a4528cba8d45322c42a7b2768acf5
Lakshya Mittal <lakshyamittalaka@gmail.com>
2025-08-09 02:22:49 +0530

    Changes for dev

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import app from "./app";
 import { Config } from "./config";
+import { AppDataSource } from "./config/data-source";
 import logger from "./config/logger";
 
-const startServer = () => {
+const startServer = async () => {
     const PORT = Config.PORT;
 
     try {
+        await AppDataSource.initialize();
+        logger.info("Database Connected Successfully");
         app.listen(PORT, () => {
             logger.info("Server listening on port", { port: PORT });
         });
